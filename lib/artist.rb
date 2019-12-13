bcaf8fb504bd2325ba30023ca9ff494ae0fd1a12
Kimberly Rouse <ksrouse94@gmail.com>
2019-12-13 06:30:38 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist 
+  attr_accessor :name
+  
+  @@all = []
+
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if @@all.include?(name)
+      
+    else
+      
+    end
+  end
+  
+  def print_songs
+    
+  end
+end
